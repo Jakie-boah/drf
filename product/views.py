e91b1fd5a04e3659d858bb4c75544fdb19d1b23c
slonn <vanogalen@yandex.ru>
2024-09-05 19:33:47 +0300

    views

--- product/views.py
+++ product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 from drf_spectacular.utils import extend_schema
 
 
@@ -16,4 +16,30 @@ class CategoryView(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandView(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing brands
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductView(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing products
+    """
+
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
