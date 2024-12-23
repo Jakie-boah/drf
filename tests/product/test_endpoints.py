f270f1944c1f2b71df761750232df5cd392ba267
slonn <vanogalen@yandex.ru>
2024-12-23 15:20:22 +0300

    full test endpoints

--- tests/product/test_endpoints.py
+++ tests/product/test_endpoints.py
@@ -16,8 +16,28 @@ class TestCategoryEndpoints:
         print(json.loads(response.content))
         assert len(json.loads(response.content)) == 4
 
-        class TestBrandEndpoints:
-            pass
 
-        class TestProductEndpoints:
-            pass
+class TestBrandEndpoints:
+    endpoint = '/api/brand/'
+
+    def test_category_get(self, brand_factory, api_client):
+        brand_factory.create_batch(4)
+
+        response = api_client().get(self.endpoint)
+
+        assert response.status_code == 200
+        print(json.loads(response.content))
+        assert len(json.loads(response.content)) == 4
+
+
+class TestProductEndpoints:
+    endpoint = '/api/product/'
+
+    def test_category_get(self, product_factory, api_client):
+        product_factory.create_batch(4)
+
+        response = api_client().get(self.endpoint)
+
+        assert response.status_code == 200
+        print(json.loads(response.content))
+        assert len(json.loads(response.content)) == 4
