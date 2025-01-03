a712eefa003c5f92c77f7843309d62414a07cce4
slonn <vanogalen@yandex.ru>
2025-01-03 16:48:09 +0300

    views

--- product/models.py
+++ product/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
@@ -22,10 +24,20 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255, null=True)
     description = models.TextField()
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    sku = models.CharField(max_length=100)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
--- product/views.py
+++ product/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import action
 
 
 class CategoryView(viewsets.ViewSet):
@@ -38,8 +38,25 @@ class ProductView(viewsets.ViewSet):
     """
 
     queryset = Product.objects.all()
+    lookup_field = 'slug'
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
+        return Response(serializer.data)
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all"
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+          An endpoint to return products by category
+          """
+        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
         return Response(serializer.data)
